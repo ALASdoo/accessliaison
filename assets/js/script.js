583d602363eb32cd552a1da46dc53ef59fb0f50a
Aleksandra Kosanovic <aleksandra@alasdoo.com>
2025-02-21 17:25:05 +0100

    Back to top button set as a link
    https://github.com/ALASdoo/accessliaison/issues/17

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,5 @@
     disable: "mobile",
   });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const backToTopBtn = document.querySelector("#back-to-top");
-
-    backToTopBtn.addEventListener("click", function (event) {
-      event.preventDefault();
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    })
-  });
 
 })();

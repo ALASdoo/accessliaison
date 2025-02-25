129ad698aa3d4680fa8a27a69322ffb985421023
Aleksandra Kosanovic <aleksandra@alasdoo.com>
2025-02-25 16:16:03 +0100

    adds event listener to the dropdown item

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,24 @@
   });
 
 
-})();
+  document.addEventListener("DOMContentLoaded", function () {
+ 
+    /**
+     * Toggle Appearance dropdown option value when user clicks on dropdown item (full with of item is clickable);
+     */
+    const appearanceButton = document.querySelector(".js--appearance-settings"); 
+    const dropdownItems = appearanceButton.querySelectorAll(".js--dropdown-item");    
+
+    dropdownItems.forEach((item) => {
+      item.addEventListener('click', (event) => { 
+        event.preventDefault();
+        const itemOption = item.querySelector('input');
+        itemOption.click(); 
+      });
+    });
+
+  });
+
+ 
+
+  })();
